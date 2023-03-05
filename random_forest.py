c3cdd96ccd6c2187dd6269718443d32eca5f66c7
brenin <marraob@gmail.com>
2023-03-05 14:30:03 +0000

    preparando dados pra rsf , ver maneiras de melhorar score

---
+++ random_forest.py
@@ -0,0 +1,63 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import numpy as np
+
+'''
+from sklearn import set_config
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+
+from sksurv.datasets import load_gbsg2
+from sksurv.preprocessing import OneHotEncoder
+from sksurv.ensemble import RandomSurvivalForest
+'''
+from sklearn.model_selection import train_test_split
+from sksurv.ensemble import RandomSurvivalForest
+
+
+filename = './lung-cancer-data.csv'
+df = pd.read_csv(filename)
+
+df['status'] = df["status"]-1
+df['sex'] = df["sex"]-1
+df['wt.loss'] = df['wt.loss'] * 0.45359237
+
+df["ph.karno"].fillna(df["ph.karno"].mean(), inplace = True)
+df["pat.karno"].fillna(df["pat.karno"].mean(), inplace = True)
+df["meal.cal"].fillna(df["meal.cal"].mean(), inplace = True)
+df["wt.loss"].fillna(df["wt.loss"].mean(), inplace = True)
+df.dropna(inplace=True)
+df["ph.ecog"] = df["ph.ecog"].astype("int64")
+
+lg_y = df[['status','time']].copy()
+
+lg_y["status"] = lg_y["status"].astype("bool") 
+
+lg_y = lg_y.to_records(index=False)
+
+lg_x = df.drop(["status","time"],axis=1)
+
+random_state = 20
+
+X_train, X_test, y_train, y_test = train_test_split(
+    lg_x, lg_y, test_size=0.25, random_state=random_state)
+
+rsf = RandomSurvivalForest(n_estimators=1000,
+                           min_samples_split=10,
+                           min_samples_leaf=15,
+                           n_jobs=-1,
+                           random_state=random_state)
+rsf.fit(X_train, y_train)
+#print(rsf.fit)
+print(rsf.score(X_test, y_test))
+'''
+rsf = RandomSurvivalForest(n_estimators=1000,
+                           min_samples_split=10,
+                           min_samples_leaf=15,
+                           n_jobs=-1,
+                           random_state=random_state)
+                           '''
+
