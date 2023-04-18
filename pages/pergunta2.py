c8d90ad11d0c6d653371e54cd6797ab8f1d4ca55
brenin <marraob@gmail.com>
2023-04-18 22:10:45 +0100

    fix pergunta2.py

--- pages/pergunta2.py
+++ pages/pergunta2.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 from lifelines import CoxPHFitter, KaplanMeierFitter
-from IPython.display import HTML
-import chart_studio as py
 import plotly.tools as tls   
 from plotly.graph_objs import *
 
