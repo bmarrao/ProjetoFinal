462ebbac523dc60795f0bf62a6bd59206afc55ce
brenin <marraob@gmail.com>
2023-04-21 17:19:01 +0100

    fiz a interação funcionar

--- pages/"Age to survival relation".py
+++ pages/"Age to survival relation".py
@@ -32,27 +32,25 @@ if st.sidebar.button('Add to graph'):
     st.session_state['pergunta1']= arr
     kmf = KaplanMeierFitter()
 
+    print(arr)
 
 
-    #Ta2 = pd.DataFrame()
+    for (n1,n2) in arr[1:]:
+        dic[f'({n1},{n2})']= {}
+        dic[f'({n1},{n2})']['status'] = []
+        dic[f'({n1},{n2})']['time'] = []
+        dic[f'({n1},{n2})']['status'] = []
+
     for index, row in df.iterrows():
         for (n1,n2) in arr[1:]:
             if row['age'] >= n1 and row['age']<= n2:
-                if dic['({n1},{n2})']:
-                    dic['({n1},{n2})']['time'].append(row['time'])
-                    dic['({n1},{n2})']['status'].append(row['status'])
-                else :
-                    dic['({n1},{n2})']['time'] =[row['time']]
-                    dic['({n1},{n2})']['status'] =[row['status']]
-
-
-
-    print(dic)
-        #print(row['time'], row['status'])
+                dic[f'({n1},{n2})']['time'].append(row['time'])
+                dic[f'({n1},{n2})']['status'].append(row['status'])
+        
 
     for (n1,n2) in arr[1:]:
         ax = plt.subplot(111)
-        kmf.fit(durations = pd.DataFrame(dic['({n1},{n2})']['time']), event_observed = pd.DataFrame(dic['({n1},{n2})']['status']),label="{num1}{num2}")
+        kmf.fit(durations = pd.DataFrame(dic[f'({n1},{n2})']['time']), event_observed = pd.DataFrame(dic[f'({n1},{n2})']['status']),label=f"{num1}-{num2}")
         kmf.survival_function_.plot(ax = ax)
 
 
@@ -69,21 +67,20 @@ if st.sidebar.button('Add to graph'):
 
 
 
+if st.button('Show age relation in cph model in a graph'):
+    cph = CoxPHFitter()
+    cph.fit(df, duration_col = 'time', event_col = 'status',formula= "age + sex + ph.ecog + ph.karno")
 
+    mpl_fig = plt.figure()
 
-cph = CoxPHFitter()
-cph.fit(df, duration_col = 'time', event_col = 'status',formula= "age + sex + ph.ecog + ph.karno")
+    cph.plot_partial_effects_on_outcome(covariates = 'age',
+                                        values = [30,40,50, 60, 70, 80],
+                                        cmap = 'coolwarm')
+                        
 
-mpl_fig = plt.figure()
+    cph2 = plt.gcf()
 
-cph.plot_partial_effects_on_outcome(covariates = 'age',
-                                    values = [30,40,50, 60, 70, 80],
-                                    cmap = 'coolwarm')
-                    
+    py_fig = tls.mpl_to_plotly(cph2, resize=True)
 
-cph2 = plt.gcf()
-
-py_fig = tls.mpl_to_plotly(cph2, resize=True)
-
-st.plotly_chart(py_fig)
+    st.plotly_chart(py_fig)
 
